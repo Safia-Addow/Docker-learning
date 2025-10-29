8dbbc498471531420cea5deb078def5c625ac45e
safia-Addow <safiaaddow@gmail.com>
2025-10-29 20:27:14 +0000

    added laod-balncer and volumes

--- Docker/coderco-challenge/count.py
+++ Docker/coderco-challenge/count.py
@@ -1,19 +1,22 @@
 from flask import Flask
 import redis
+import os
 
 app = Flask(__name__)
 
-redis_client = redis.StrictRedis(host='redis', port=6379, decode_responses=True)
+# Use environment variables with fallback defaults
+redis_host = os.getenv('REDIS_HOST', 'redis')
+redis_port = int(os.getenv('REDIS_PORT', 6379))
+r = redis.Redis(host=redis_host, port=redis_port, decode_responses=True)
 
 @app.route('/')
-def welcome():
+def home():
     return "✅ Welcome! Flask + Redis coderco challenge!"
 
 @app.route('/count')
 def count():
-    count=redis_client.incr("visits")
-   # visits = redis_client.get("visits")
-    return f"🔢 this Page has been visited {count} times."
+    visits = r.incr("visits")
+    return f"🔢 This page has been visited {visits} times."
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
